87155cad431bf532264b6afc2dce1459199b01e2
Alexander Gude <alex.public.account@gmail.com>
2019-05-19 20:20:00 -0700

    Bump version to 0.7.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
 # Configure the package
 setup(
     name="Github Pages Archiver",
-    version="0.6.0",
+    version="0.7.0",
     description="A script to backup Github Pages using the Internet Archive",
     long_description=long_description,
     author="Alexander Gude",
