437f1d75dfe7fd3c13f7f9999d4b0b7487bd1f85
Jacopo <5214104+stjaco@users.noreply.github.com>
2019-11-20 17:25:56 +0100

    allowed redirect in head call

--- wayback_machine_archiver/archiver.py
+++ wayback_machine_archiver/archiver.py
@@ -30,7 +30,7 @@ def call_archiver(request_url, rate_limit_wait, session):
         logging.debug("Sleeping for %s", rate_limit_wait)
         time.sleep(rate_limit_wait)
     logging.info("Calling archive url %s", request_url)
-    r = session.head(request_url)
+    r = session.head(request_url, allow_redirects=True)
     try:
         # Raise `requests.exceptions.HTTPError` if 4XX or 5XX status
         r.raise_for_status()
